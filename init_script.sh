a06cd284866e1c4a0379a5975dba4e3143b6bc7a
dcrodjer <dcrodjer@gmail.com>
2011-01-07 22:03:37 +0530

    Added chown - www-data command for files directory in init_script

--- init_script.sh
+++ init_script.sh
@@ -2,3 +2,4 @@ mkdir web_interface/files
 mkdir web_interface/files/inputs web_interface/files/outputs web_interface/files/programs web_interface/files/temp 
 cp -r web_interface/files/inputs web_interface/files/outputs web_interface/files/programs web_interface/files/temp/
 cp -r web_interface/files worker_config
+chown -R www-data web_interface/files/
