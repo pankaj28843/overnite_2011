f0c34928218d521619db1ed62321cd4036725a98
dcrodjer <dcrodjer@gmail.com>
2011-01-12 22:00:17 +0530

    Wysiwig editor settings fix.

--- web_interface/media/js/admin_wysiwg.js
+++ web_interface/media/js/admin_wysiwg.js
@@ -1,1 +1,1 @@
-bkLib.onDomLoaded(function() { nicEditors.allTextAreas({buttonList : ['bold','underline','left','center','right','strikeThrough','subscript','superscript','fontSize', 'fontFormat', 'link','unlink','forecolor','bgcolor','image','upload','removeformat']} ) });
+bkLib.onDomLoaded(function() { nicEditors.allTextAreas({buttonList : ['bold','underline','left','center','right','strikeThrough','subscript','superscript', 'ol', 'ul', 'fontFormat', 'link','unlink','forecolor','bgcolor','image','upload','removeformat','xhtml']} ) });
